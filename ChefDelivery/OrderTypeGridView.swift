16cba36a395bb5ba7322064506657092cbc2b56c
Abiscula <victorabiscula@live.com>
2024-03-24 15:03:21 -0300

    Criado arquivo de mock para representar as opcoes do app

--- ChefDelivery/OrderTypeGridView.swift
+++ ChefDelivery/OrderTypeGridView.swift
@@ -9,16 +9,14 @@ import SwiftUI
 
 struct OrderTypeGridView: View {
   
-  let  orders = ["Restaurantes", "Farmácia", "Descontos", "Gourmet", "Mercados" , "Pet", "Bebidas"]
-  
   var body: some View {
     
     LazyHGrid(rows: [
       GridItem(.fixed(100)),
       GridItem(.fixed(100))
     ], content: {
-      ForEach(orders, id: \.self) { ordemItem in
-        Text(ordemItem)
+      ForEach(ordersMock) { ordemItem in
+        Text(ordemItem.name)
       }
     })
     
