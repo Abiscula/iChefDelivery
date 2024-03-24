6598507a505b54edd9cb625bf905dedf9290b4c0
Abiscula <victorabiscula@live.com>
2024-03-24 17:34:07 -0300

    Criada nova view para representar cada item do OrderTypeGridView

---
+++ ChefDelivery/GridView/OrderTypeView.swift
@@ -0,0 +1,29 @@
+//
+//  OrderTypeView.swift
+//  ChefDelivery
+//
+//  Created by Victor Abiscula on 24/03/24.
+//
+
+import SwiftUI
+
+struct OrderTypeView: View {
+  
+  let orderType: OrderType
+  
+    var body: some View {
+      VStack(spacing: 5) {
+        Image(orderType.image)
+          .resizable() // usado para indicar que a imagem pode ser redimensionada
+          .scaledToFit() // usado para dimensionar a imagem para caber dentro do espaço disponível
+          .fixedSize(horizontal: false, vertical: /*@START_MENU_TOKEN@*/true/*@END_MENU_TOKEN@*/)
+        Text(orderType.name)
+          .font(.system(size: 10))
+      }
+      .frame(width: 70, height: 100)
+    }
+}
+
+#Preview("OrderTypeView", traits: .sizeThatFitsLayout) {
+  OrderTypeView(orderType: OrderType(id: 1, name: "Restaurantes", image: "hamburguer"))
+}
