99f21834767d00ade7dd305af9d74130b2927e79
Abiscula <victorabiscula@live.com>
2024-03-26 23:07:29 -0300

    Iniciada criação do componente de carousel

--- ChefDelivery/CarouselView/CarouselItemView.swift
+++ ChefDelivery/CarouselView/CarouselItemView.swift
@@ -8,11 +8,19 @@
 import SwiftUI
 
 struct CarouselItemView: View {
+  
+  let order: OrderType
+  
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+      Image(order.image)
+        .resizable()
+        .scaledToFit()
+        .cornerRadius(12)
+      
     }
 }
 
-#Preview {
-    CarouselItemView()
+#Preview("CarouselItemView", traits: .sizeThatFitsLayout) {
+  CarouselItemView(order: OrderType(id: 1, name: "", image: "barbecue-banner"))
+    .padding()
 }
