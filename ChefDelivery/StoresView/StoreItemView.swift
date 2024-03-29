befaacbf84a61a98c302a2cb5bd01fe7d580fdaf
Abiscula <victorabiscula@live.com>
2024-03-29 13:38:09 -0300

    Adicionado spacer para que o componente respeite o alinhamento proposto no StoresContainerView

--- ChefDelivery/StoresView/StoreItemView.swift
+++ ChefDelivery/StoresView/StoreItemView.swift
@@ -8,25 +8,30 @@
 import SwiftUI
 
 struct StoreItemView: View {
-  
-  let order: OrderType
-  
+    
+    let order: OrderType
+    
     var body: some View {
-      HStack {
-        Image(order.image)
-          .resizable()
-          .scaledToFit()
-          .cornerRadius(25)
-          .frame(width: 50, height: 50)
-        
-        VStack {
-          Text(order.name)
-            .font(.subheadline)
+        HStack {
+            Image(order.image)
+                .resizable()
+                .scaledToFit()
+                .cornerRadius(25)
+                .frame(width: 50, height: 50)
+            
+            VStack {
+                Text(order.name)
+                    .font(.subheadline)
+            }
+            
+            Spacer()
         }
-      }
+        .onTapGesture {
+            print("Clicou em \(order.name)")
+        }
     }
 }
 
 #Preview("StoreItemView", traits: .sizeThatFitsLayout) {
-  StoreItemView(order: OrderType(id: 1, name: "Monstro Burguer", image: "monstro-burger-logo"))
+    StoreItemView(order: OrderType(id: 1, name: "Monstro Burguer", image: "monstro-burger-logo"))
 }
