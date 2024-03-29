886742af4b2da4f99e4a7bcbb219a34d8c5f5cd3
Abiscula <victorabiscula@live.com>
2024-03-29 12:44:09 -0300

    Criado novo mock para renderização das lojas

--- ChefDelivery/Model/DataSourceMock.swift
+++ ChefDelivery/Model/DataSourceMock.swift
@@ -17,3 +17,11 @@ let ordersMock: [OrderType] = [
   OrderType(id: 7, name: "Gourmet", image: "gourmet"),
 ]
 
+let storesMock: [OrderType] = [
+  OrderType(id: 1, name: "Monster Burguer", image: "monstro-burger-logo"),
+  OrderType(id: 2, name: "Food court", image: "food-court-logo"),
+  OrderType(id: 3, name: "Carbron", image: "carbron-logo"),
+  OrderType(id: 4, name: "Padaria", image: "bakery-logo"),
+  OrderType(id: 5, name: "Açai Panda", image: "acai-panda-logo"),
+]
+
---
+++ ChefDelivery/StoresView/StoresContainerView.swift
@@ -0,0 +1,18 @@
+//
+//  StoresContainerView.swift
+//  ChefDelivery
+//
+//  Created by Victor Abiscula on 29/03/24.
+//
+
+import SwiftUI
+
+struct StoresContainerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StoresContainerView()
+}
