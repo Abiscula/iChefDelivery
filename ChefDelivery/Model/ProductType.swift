0dc69cdd6787bf946dc47694178b7180806f9ceb
Abiscula <victorabiscula@live.com>
2024-03-29 19:19:23 -0300

    Criada nova struct para produto

---
+++ ChefDelivery/Model/ProductType.swift
@@ -0,0 +1,16 @@
+//
+//  ProductType.swift
+//  ChefDelivery
+//
+//  Created by Victor Abiscula on 29/03/24.
+//
+
+import Foundation
+
+struct ProductType: Identifiable {
+    let id: Int
+    let name: String
+    let description: String
+    let image: String
+    let price: Double
+}
---
+++ ChefDelivery/Model/StoreType.swift
@@ -0,0 +1,8 @@
+//
+//  StoreType.swift
+//  ChefDelivery
+//
+//  Created by Victor Abiscula on 29/03/24.
+//
+
+import Foundation
