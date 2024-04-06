3ae66d027430525534a4a32102408e22320f0b3b
Abiscula <victorabiscula@live.com>
2024-04-06 20:03:30 -0300

    Adicionado componente CircleView a HomeView

--- ChefDelivery/HomeView/HomeView.swift
+++ ChefDelivery/HomeView/HomeView.swift
@@ -12,27 +12,46 @@ struct HomeView: View {
     @State private var isAnimating = false
     
     var body: some View {
-        VStack {
-            Text("Chef Delivery")
-                .font(.system(size: 40))
-                .fontWeight(.heavy)
-                .foregroundColor(Color("ColorRed"))
-                .opacity(isAnimating ? 1 : 0)
-                .offset(y: isAnimating ? 0 : -40)
-            
-            Text("Peça as suas comidas no conforto da sua casa.")
-                .font(.title2)
-                .padding()
-                .multilineTextAlignment(.center)
-                .foregroundColor(.black.opacity(0.7))
-                .opacity(isAnimating ? 1 : 0)
-                .offset(y: isAnimating ? 0 : -40)
-            
-            Spacer()
-        }
-        .onAppear {
-            withAnimation(.easeInOut(duration: 1.5)) {
-                isAnimating = true
+        
+        GeometryReader { geometry in
+            ZStack {
+                
+                CircleView(
+                    color: Color("ColorRed"),
+                    positionX: isAnimating ? 50 : -50,
+                    positionY: isAnimating ? 100 : -100,
+                    hasAnimating: isAnimating)
+                
+                CircleView(
+                    color: Color("ColorRedDark"),
+                    positionX: (isAnimating ? geometry.size.width - 50 : geometry.size.width + 50),
+                    positionY: (isAnimating ? geometry.size.height - 50 : geometry.size.height + 50),
+                    hasAnimating: isAnimating)
+                
+                
+                VStack {
+                    Text("Chef Delivery")
+                        .font(.system(size: 40))
+                        .fontWeight(.heavy)
+                        .foregroundColor(Color("ColorRed"))
+                        .opacity(isAnimating ? 1 : 0)
+                        .offset(y: isAnimating ? 0 : -40)
+                    
+                    Text("Peça as suas comidas no conforto da sua casa.")
+                        .font(.title2)
+                        .padding()
+                        .multilineTextAlignment(.center)
+                        .foregroundColor(.black.opacity(0.7))
+                        .opacity(isAnimating ? 1 : 0)
+                        .offset(y: isAnimating ? 0 : -40)
+                    
+                    Spacer()
+                }
+                .onAppear {
+                    withAnimation(.easeInOut(duration: 1.5)) {
+                        isAnimating = true
+                    }
+            }
             }
         }
     }
