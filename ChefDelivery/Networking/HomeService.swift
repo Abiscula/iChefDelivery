ba43097488b89390cbffcec19df6c39493a330ce
Abiscula <victorabiscula@live.com>
2024-05-13 19:53:57 -0300

    Criada nova request para persistir o produto selecionado pelo usuário
    Implementado o protocolo codable em ProductType

--- ChefDelivery/Networking/HomeService.swift
+++ ChefDelivery/Networking/HomeService.swift
@@ -28,4 +28,21 @@ struct HomeService {
         return .success(storeObjects)
     }
     
+    func confirmOrder(product: ProductType) async throws -> Result<[String: Any]?, RequestError> {
+        guard let url = URL(string: "https://private-425152-chefdelivery11.apiary-mock.com/home") else {
+            return .failure(.invalidURL)
+        }
+        
+        let encodedObject = try JSONEncoder().encode(product)
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.httpBody = encodedObject
+        
+        let (data, _) = try await URLSession.shared.data(for: request)
+        let message = try JSONSerialization.jsonObject(with: data) as? [String: Any]
+        
+        return .success(message)
+    }
+    
 }
